4af9a262d832ea6ba6ad12d9090e14c5b931dcda
Renato Medina <renatomedina.dev@gmail.com>
2025-04-08 17:00:11 -0500

    feat: add metodos al interface EstudianteRepository.java

--- sistema-pagos-backend/src/main/java/com/lta/sistemapagos/repository/EstudianteRepository.java
+++ sistema-pagos-backend/src/main/java/com/lta/sistemapagos/repository/EstudianteRepository.java
@@ -1,5 +1,14 @@
 package com.lta.sistemapagos.repository;
 
-public interface EstudianteRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lta.sistemapagos.entities.Estudiante;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante,String>{
+
+    Estudiante findByCodigo(String codigo);
+
+    List<Estudiante> findByProgramaId(String programaId);
 }
