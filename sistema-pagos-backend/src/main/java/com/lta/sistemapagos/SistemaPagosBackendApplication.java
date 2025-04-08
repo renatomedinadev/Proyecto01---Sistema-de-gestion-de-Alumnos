6cad3029af8f787f82d07596261f5a2d9db7b113
Renato Medina <renatomedina.dev@gmail.com>
2025-04-08 15:53:40 -0500

    Primer Commit: Spring Initializr

---
+++ sistema-pagos-backend/src/main/java/com/lta/sistemapagos/SistemaPagosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lta.sistemapagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPagosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPagosBackendApplication.class, args);
+	}
+
+}
