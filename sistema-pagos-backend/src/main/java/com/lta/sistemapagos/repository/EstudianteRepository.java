e598d96781b4bd8eb89300245559d32b5ccd312c
Renato Medina <renatomedina.dev@gmail.com>
2025-04-08 17:36:01 -0500

    feat: add metodo al interface PagoRepository.java

--- sistema-pagos-backend/src/main/java/com/lta/sistemapagos/repository/EstudianteRepository.java
+++ sistema-pagos-backend/src/main/java/com/lta/sistemapagos/repository/EstudianteRepository.java
@@ -3,9 +3,11 @@ package com.lta.sistemapagos.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lta.sistemapagos.entities.Estudiante;
 
+@Repository
 public interface EstudianteRepository extends JpaRepository<Estudiante,String>{
 
     Estudiante findByCodigo(String codigo);
--- sistema-pagos-backend/src/main/java/com/lta/sistemapagos/repository/PagoRepository.java
+++ sistema-pagos-backend/src/main/java/com/lta/sistemapagos/repository/PagoRepository.java
@@ -1,6 +1,20 @@
 package com.lta.sistemapagos.repository;
 
-public interface PagoRepository {
+import java.util.List;
 
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lta.sistemapagos.entities.Pago;
+import com.lta.sistemapagos.enums.PagoStatus;
+import com.lta.sistemapagos.enums.TypePago;
+
+@Repository
+public interface PagoRepository extends JpaRepository<Pago,Long>{
+
+    List<Pago> findByEstudianteCodigo(String codigo);
+
+    List<Pago> findByStatus(PagoStatus status);
+
+    List<Pago> findByType(TypePago typePago);
 }
