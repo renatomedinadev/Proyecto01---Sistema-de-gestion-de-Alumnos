939f334fe9e47e656394804f9ec870441b2af221
Renato Medina <renatomedina.dev@gmail.com>
2025-04-08 16:40:32 -0500

    feat: add propiedades de la entidad Pago.java

--- sistema-pagos-backend/src/main/java/com/lta/sistemapagos/entities/Pago.java
+++ sistema-pagos-backend/src/main/java/com/lta/sistemapagos/entities/Pago.java
@@ -1,5 +1,42 @@
 package com.lta.sistemapagos.entities;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDate;
+
+import com.lta.sistemapagos.enums.PagoStatus;
+import com.lta.sistemapagos.enums.TypePago;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pago {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private LocalDate fecha;
+
+    private double cantidad;
+
+    private TypePago type;
+
+    private PagoStatus status;
+
+    private String file;
+
+    @ManyToOne
+    private Estudiante estudiante;
 }
