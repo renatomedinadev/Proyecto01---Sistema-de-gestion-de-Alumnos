b5b3323e0d90ab566b3642e10f1d9c247189c9ea
Renato Medina <renatomedina.dev@gmail.com>
2025-04-09 13:05:21 -0500

    feat: add creamos los metodos Put y Post en PagoController.java

--- sistema-pagos-backend/src/main/java/com/lta/sistemapagos/web/PagoController.java
+++ sistema-pagos-backend/src/main/java/com/lta/sistemapagos/web/PagoController.java
@@ -1,20 +1,29 @@
 package com.lta.sistemapagos.web;
 
+
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.lta.sistemapagos.entities.Estudiante;
 import com.lta.sistemapagos.entities.Pago;
 import com.lta.sistemapagos.enums.PagoStatus;
+import com.lta.sistemapagos.enums.TypePago;
 import com.lta.sistemapagos.repository.EstudianteRepository;
 import com.lta.sistemapagos.repository.PagoRepository;
 import com.lta.sistemapagos.services.PagoService;
+
 
 @RestController
 @CrossOrigin("*")
@@ -64,9 +73,25 @@ public class PagoController {
         return pagoRepository.findByStatus(status);
     }
 
-    @
+    @GetMapping("/pagos/porTipo")
+    public List<Pago> listarPagosPorType(@RequestParam TypePago type){
+        return pagoRepository.findByType(type);
+    }
+
+
+    @PutMapping("/pagos/{pagoId}/actualizarPago")
     public Pago actualizarStatusDePago(@RequestParam PagoStatus status,@PathVariable Long pagoId){
-        
+        return pagoService.actualizarPagoPorStatus(status, pagoId);
+    }
+
+    @PostMapping(path = "/pagos",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Pago guardarPago(@RequestParam("file") MultipartFile file, double cantidad, TypePago type, LocalDate date, String codigoEstudiante) throws IOException{
+        return pagoService.savePago(file, cantidad, type, date, codigoEstudiante);
+    }
+
+    @GetMapping(value = "/pagoFile/{pagoId}",produces = MediaType.APPLICATION_PDF_VALUE)
+    public byte[] listarArchivoPorId(@PathVariable Long pagoId) throws IOException{
+        return pagoService.getArchivoPorId(pagoId);
     }
 }
 
