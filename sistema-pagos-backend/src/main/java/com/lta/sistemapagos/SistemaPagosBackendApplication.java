78c0be87069282c17782924f9eb3dd7e0e7578be
Renato Medina <renatomedina.dev@gmail.com>
2025-04-09 16:08:43 -0500

    docs: agregar documentación Swagger - cargando datos de prueba

--- sistema-pagos-backend/src/main/java/com/lta/sistemapagos/SistemaPagosBackendApplication.java
+++ sistema-pagos-backend/src/main/java/com/lta/sistemapagos/SistemaPagosBackendApplication.java
@@ -1,5 +1,7 @@
 package com.lta.sistemapagos;
 
+import java.time.LocalDate;
+import java.util.Random;
 import java.util.UUID;
 
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.lta.sistemapagos.entities.Estudiante;
+import com.lta.sistemapagos.entities.Pago;
+import com.lta.sistemapagos.enums.PagoStatus;
+import com.lta.sistemapagos.enums.TypePago;
 import com.lta.sistemapagos.repository.EstudianteRepository;
 import com.lta.sistemapagos.repository.PagoRepository;
 
@@ -19,40 +24,56 @@ public class SistemaPagosBackendApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(EstudianteRepository estudianteRepository, PagoRepository pagoRepository){
+	CommandLineRunner commandLineRunner(EstudianteRepository estudianteRepository, PagoRepository pagoRepository) {
 		return args -> {
 			estudianteRepository.save(Estudiante.builder()
-			.id(UUID.randomUUID().toString())
-			.nombre("Christian")
-			.apellido("Ramirez")
-			.codigo("1234")
-			.programaId("LTA1")
-			.build());
+					.id(UUID.randomUUID().toString())
+					.nombre("Christian")
+					.apellido("Ramirez")
+					.codigo("1234")
+					.programaId("LTA1")
+					.build());
 
 			estudianteRepository.save(Estudiante.builder()
-			.id(UUID.randomUUID().toString())
-			.nombre("Biaggio")
-			.apellido("Ramirez")
-			.codigo("12354")
-			.programaId("LTA1")
-			.build());
+					.id(UUID.randomUUID().toString())
+					.nombre("Biaggio")
+					.apellido("Ramirez")
+					.codigo("12354")
+					.programaId("LTA1")
+					.build());
 
 			estudianteRepository.save(Estudiante.builder()
-			.id(UUID.randomUUID().toString())
-			.nombre("Julen")
-			.apellido("Ramirez")
-			.codigo("1256634")
-			.programaId("LTA1")
-			.build());
+					.id(UUID.randomUUID().toString())
+					.nombre("Julen")
+					.apellido("Ramirez")
+					.codigo("1256634")
+					.programaId("LTA1")
+					.build());
 
 			estudianteRepository.save(Estudiante.builder()
-			.id(UUID.randomUUID().toString())
-			.nombre("Raul")
-			.apellido("Ramirez")
-			.codigo("12349030")
-			.programaId("LTA2")
-			.build());
+					.id(UUID.randomUUID().toString())
+					.nombre("Raul")
+					.apellido("Ramirez")
+					.codigo("12349030")
+					.programaId("LTA2")
+					.build());
+
+			TypePago tiposPago[] = TypePago.values();
+			Random random = new Random();
+
+			estudianteRepository.findAll().forEach(estudiante -> {
+				for (int i = 0; i < 10; i++) {
+					int index = random.nextInt(tiposPago.length);
+					Pago pago = Pago.builder()
+							.cantidad(1000 + (int) (Math.random() * 20000))
+							.type(tiposPago[index])
+							.status(PagoStatus.CREADO)
+							.fecha(LocalDate.now())
+							.estudiante(estudiante)
+							.build();
+					pagoRepository.save(pago);
+				}
+			});
 		};
 	}
-
 }
--- sistema-pagos-backend/src/main/java/com/lta/sistemapagos/entities/Pago.java
+++ sistema-pagos-backend/src/main/java/com/lta/sistemapagos/entities/Pago.java
@@ -25,7 +25,7 @@ public class Pago {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     private LocalDate fecha;
 
