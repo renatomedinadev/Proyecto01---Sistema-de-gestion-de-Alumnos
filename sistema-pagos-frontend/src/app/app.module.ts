9bd51c2d9cf65fcc6ddab267733b545dd872ff36
Renato Medina <renatomedina.dev@gmail.com>
2025-04-11 13:12:44 -0500

    feat: crear el frontend del proyecto con Angular

---
+++ sistema-pagos-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminTemplateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
